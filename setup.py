0bd6c1ef131ab0abe533a87a3ba0a5f768f4b581
riddhi183 <riddhi183@gmail.com>
2020-08-17 23:19:56 -0400

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="SE20 Group 11",
+    version="0.0.1",
+    author="SE20 Group 11",
+    author_email="riddhi183@gmail.com",
+    description="This is Group 11 homework 1 for CS510 Software Engineering",
+    url="https://github.com/Parth27/SE_Homework1",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
